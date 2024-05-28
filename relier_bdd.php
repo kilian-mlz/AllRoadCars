7553bd7b9b9a87db52f724651f1b70766dc21283
kilian-mlz <154443729+kilian-mlz@users.noreply.github.com>
2024-05-28 22:39:11 +0200

    Update relier_bdd.php

--- relier_bdd.php
+++ relier_bdd.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; // Adresse du serveur MySQL
-$username = "root"; // Nom d'utilisateur de la base de données
-$password = ""; // Mot de passe de la base de données
-$dbname = "forum"; // Nom de la base de données
+$username = "john"; // Nom d'utilisateur de la base de données
+$password = "password123"; // Mot de passe de la base de données
+$dbname = "blog"; // Nom de la base de données
 
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
